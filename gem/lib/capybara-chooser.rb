7e639694e2147a30a94d5973997d6d2ff0c2ba70
Damien Szczyt <damien.szczyt@geniustrade.com>
2014-04-17 16:28:51 +0200

    update capybara to use latest chooser

--- gem/lib/capybara-chooser.rb
+++ gem/lib/capybara-chooser.rb
@@ -9,7 +9,7 @@ module Capybara
       chooser_container = first(:xpath, xpath) if xpath.present?
       chooser_container ||= first('label', text: from).find(:xpath, '..').find '.chooser-container'
 
-      chooser_container.find('.chooser-selected').click
+      chooser_container.find('.chooser-field').click
 
       if search
         chooser_search = 'input.chooser-search'
@@ -19,40 +19,6 @@ module Capybara
 
       drop_container = '.chooser-search-results'
       find(:xpath, '//body').find("#{drop_container} li", text: value).click
-
-      fail unless page.has_no_css? '#chooser-drop-mask'
-    end
-
-    # def chooser_ajax(value, from: nil, xpath: nil, search: false, parent: '')
-    #   fail "Must pass a hash containing 'from' or 'xpath'" unless from.present? || xpath.present?
-
-    #   chooser_container = first(:xpath, xpath) if xpath.present?
-    #   chooser_container ||= first('label', text: from).find(:xpath, '..').find '.chooser-container'
-
-    #   chooser_container.find('.chooser-choice').click
-
-    #   if search
-    #     chooser_input = "#{parent} input.chooser-input".strip
-    #     find(:xpath, '//body').find(chooser_input).set value
-    #     page.execute_script(%|$("#{chooser_input}:visible").keyup();|)
-    #   end
-
-    #   drop_container = search ? '.chooser-results' : '.chooser-drop'
-    #   find(:xpath, '//body').find("#{drop_container} li", text: value).click
-
-    #   fail unless page.has_no_css? '#chooser-drop-mask'
-    # end
-
-    def chooser_multiple(values, from: nil, xpath: nil)
-      fail "Must pass a hash containing 'from' or 'xpath'" unless from.present? || xpath.present?
-
-      chooser_container = first(:xpath, xpath) if xpath.present?
-      chooser_container ||= first('label', text: from).find(:xpath, '..').find '.chooser-container'
-
-      [values].flatten.each do |value|
-        chooser_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' chooser-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' chooser-choices ')]").click
-        find(:xpath, '//body').find('.chooser-drop li', text: value).click
-      end
 
       fail unless page.has_no_css? '#chooser-drop-mask'
     end
